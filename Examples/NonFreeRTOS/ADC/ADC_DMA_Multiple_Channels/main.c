5727ebf57977f6b5ff014f0772326a8319bee021
IOsetting <iosetting@outlook.com>
2022-10-07 20:27:06 +0800

    feat: adc dma examples

---
+++ Examples/NonFreeRTOS/ADC/ADC_DMA_Multiple_Channels/main.c
@@ -0,0 +1,103 @@
+
+#include <air32f10x_adc.h>
+#include <air32f10x_dma.h>
+#include "debug.h"
+
+
+uint16_t TxBuf[1024];
+
+
+void ADC_Function_Init(void)
+{
+    ADC_InitTypeDef ADC_InitStructure;
+    GPIO_InitTypeDef GPIO_InitStructure;
+    // Enable periphal clock
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+    // ADC clock =  PCLK2 / 8
+    RCC_ADCCLKConfig(RCC_PCLK2_Div8);
+    // GPIO: A0, A1, A2 as analog input
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    // Reset ADC1
+    ADC_DeInit(ADC1);
+    // Independent mod, continuous, 3 channels
+    ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+    ADC_InitStructure.ADC_ScanConvMode = ENABLE;
+    ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+    ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+    ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+    ADC_InitStructure.ADC_NbrOfChannel = 3;
+    ADC_Init(ADC1, &ADC_InitStructure);
+    // Enable ADC1 with DMA
+    ADC_DMACmd(ADC1, ENABLE);
+    ADC_Cmd(ADC1, ENABLE);
+    // Calibration
+    ADC_ResetCalibration(ADC1);
+    while (ADC_GetResetCalibrationStatus(ADC1));
+    ADC_StartCalibration(ADC1);
+    while (ADC_GetCalibrationStatus(ADC1));
+}
+
+/*******************************************************************************
+* Function Name  : DMA_Tx_Init
+* Description    : Initializes the DMA Channelx configuration.
+* Input          : DMA_CHx:
+*                    x can be 1 to 7.
+*                  ppadr: Peripheral base address.
+*                  memadr: Memory base address.
+*                  bufsize: DMA channel buffer size.
+* Return         : None
+*******************************************************************************/
+void DMA_Tx_Init(DMA_Channel_TypeDef *DMA_CHx, uint32_t ppadr, uint32_t memadr, uint16_t bufsize)
+{
+    DMA_InitTypeDef DMA_InitStructure;
+
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+
+    DMA_DeInit(DMA_CHx);
+    DMA_InitStructure.DMA_PeripheralBaseAddr = ppadr;
+    DMA_InitStructure.DMA_MemoryBaseAddr = memadr;
+    DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+    DMA_InitStructure.DMA_BufferSize = bufsize;
+    // Addresss increase - peripheral:no, memory:yes
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    // Data unit size: 16bit
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+    DMA_InitStructure.DMA_Priority = DMA_Priority_Medium;
+    // Memory to memory: no
+    DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+    DMA_Init(DMA_CHx, &DMA_InitStructure);
+}
+
+int main(void)
+{
+    uint16_t i;
+
+    Delay_Init();
+    USART_Printf_Init(115200);
+    printf("SystemClk:%ld\r\n", SystemCoreClock);
+
+    ADC_Function_Init();
+    DMA_Tx_Init(DMA1_Channel1, (uint32_t)&ADC1->DR, (uint32_t)TxBuf, 1024);
+    DMA_Cmd(DMA1_Channel1, ENABLE);
+
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_239Cycles5);
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 2, ADC_SampleTime_239Cycles5);
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_2, 3, ADC_SampleTime_239Cycles5);
+    // Run ADC for 50 milliseconds
+    ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+    Delay_Ms(50);
+    ADC_SoftwareStartConvCmd(ADC1, DISABLE);
+
+    for (i = 0; i < 1024; i++)
+    {
+        printf("%04d\r\n", TxBuf[i]);
+        Delay_Ms(10);
+    }
+    while (1);
+}
